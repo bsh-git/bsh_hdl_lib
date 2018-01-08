08ddae74369854117417f20e7ad28da07c76882a
Hiroyuki Bessho <bessho@genetec.co.jp>
2018-01-08 22:55:32 +0900

    handle LED width correctly

--- knight/knight.v
+++ knight/knight.v
@@ -5,8 +5,8 @@
 // 
 // Create Date: 12/29/2017 11:32:38 AM
 // Design Name: 
-// Module Name: led
-// Project Name: 
+// Module Name: knight
+// Project Name: knight rider pattern
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -20,57 +20,51 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module knight_core # (
+module knight # (
 		 parameter integer WIDTH = 8
 		 )
    (
-    output reg [WIDTH-1:0]  led,
-    input 	      clk_src,
-    input wire [23:0] divider,
+    output reg [WIDTH-1:0] led,
+    input 		   clk_src,
+    input wire [23:0] 	   divider
     //	   input 	    reset
     );
    
 
    reg 			dir;
-
    wire 		clk;
    
 
-//   fixed_divider #(24, 24'h7fffff) div(.clk_in(clk_src), .clk_out(clk));
    variable_divider #(24) div(.clk_in(clk_src), .load(divider), .clk_out(clk));
 
-   always @(posedge clk or posedge reset) begin
-      if (reset) begin
-	 led <= 8'h5a;
-	 dir <= 1;
+   always @(posedge clk) begin
+      if (dir == 1) begin
+	 if (led == 0)
+	   led <= 1;
+	 else if (led == 1)
+	   led <= 2'b11;
+	 else if (led[WIDTH-1:WIDTH-2] == 2'b10) begin
+	    led <= 0;
+	    dir <= 0;
+	 end
+	 else
+	   led <= led << 1;
       end
       else begin
-	 if (dir == 1) begin
-	    if (led == 0)
-	      led <= 1;
-	    else if (led == 8'b00000001)
-	      led <= 8'b00000011;
-	    else if (led == 8'b10000000) begin
-	       led <= 0;
-	       dir <= 0;
-	    end
-	    else
-	      led <= led << 1;
+	 if (led == 0) begin
+	    led[WIDTH-2:0] <= 0;
+	    led[WIDTH-1] <= 1;
 	 end
-	 else begin
-	    if (led == 0)
-	      led <= 8'b10000000;
-	    else if (led == 8'b10000000)
-	      led <= 8'b11000000;
-	    else if (led == 8'b00000001) begin
-	       led <= 0;
-	       dir <= 1;
-	    end
-	    else
-	      led <= led >> 1;
+	 else if (led[WIDTH-1:WIDTH-2] == 2'b10)
+	   led[WIDTH-1:WIDTH-2] <= 2'b11;
+	 else if (led == 1) begin
+	    led <= 0;
+	    dir <= 1;
 	 end
+	 else
+	   led <= led >> 1;
       end
-
    end
+
  			
 endmodule
