773bdbe528a25104fea0a2e7c1456b4147aed712
Hiroyuki Bessho <bsh@binchodeb.genetec.local>
2018-01-15 16:50:45 +0900

    employ an alternative implementation.

--- waves/triangle_wave.v
+++ waves/triangle_wave.v
@@ -5,6 +5,61 @@
 `define UP	0
 `define DOWN	1
 
+//`define ALTERNATIVE
+
+`ifndef ALTERNATIVE
+module triangle_wave #
+  (
+   parameter integer WIDTH = 8
+   )
+   (
+    input 	       clk,
+    input 	       enable,
+    input [WIDTH-1:0]  low_in,
+    input [WIDTH-1:0]  high_in,
+    output [WIDTH-1:0] mod_out
+    );
+
+   reg [WIDTH-1:0] counter;
+   reg 		   dir = `UP;
+
+   assign mod_out = counter;
+
+   always @(posedge enable) begin
+      counter <= low_in;
+   end
+
+   always @(posedge clk) begin
+
+      if (enable) begin
+
+	 if (dir == `DOWN) begin
+	    if (counter > low_in)
+	      counter <= counter - 1;
+	    else begin
+	       dir <= `UP;
+	       counter <= counter + 1;
+	    end
+	 end else begin
+	    if (counter < high_in)
+	      counter <= counter + 1;
+	    else begin
+	       dir <= `DOWN;
+	       counter <= counter - 1;
+	    end
+	 end // else: !if(dir == `DOWN)
+      end
+   end // always @ (posedge pwmlast)
+
+endmodule // triangle_wave
+
+`else
+
+/*
+ カウンターと low, high を比較する時、不等号比較よりも等号比較の方が回路が簡単ではあるが、
+ 波形の途中で low, high が変更された場合におかしくならないようにするために余分な工夫が必要ななる。
+ 合成後のセル数では、こちらの方が大きくなってしまった。
+*/
 module triangle_wave #
   (
    parameter integer WIDTH = 8
@@ -57,3 +112,4 @@ module triangle_wave #
 
 endmodule
 
+`endif
